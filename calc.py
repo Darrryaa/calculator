30478400995881b797794494c62028ffe09f79d4
Darrryaa <159245758+Darrryaa@users.noreply.github.com>
2024-03-19 22:27:04 +0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,24 +1,32 @@
 import tkinter as tk
-import math
+import math 
 
-def add_digit(operation, digit, input_l):
+
+def add_digit(operation, digit, input_l, label = None):
     match operation:
         case "add":
             curent = input_l.get() 
             input_l.delete(0, tk.END) 
-            input_l.insert(0, curent + str(digit)) 
+            input_l.insert(0, curent + str(digit))
         case "erase":
             curent = input_l.get()
-            input_l.delete(0, tk.END) 
+            input_l.delete(0, tk.END)  
             input_l.insert(0, curent[:-1])
+        case "erase_all":
+            curent = input_l.get()
+            input_l.delete(0, tk.END) 
+            label.config(text= f"Результат: ")
+
 
 def calculate(input_l, label):
-    current = input_l.get()
+    curent = input_l.get()
     try:
-        result = eval(current) 
+        result = eval(curent) 
         label.config(text=f"Делить на ноль нельзя!" if error(result) else f"Результат: {result}")
     except ZeroDivisionError:
         label.config(text="Делить на ноль нельзя!")
+    except SyntaxError:
+        label.config(text="Результат: ")
 
 
 def error(result):
