018800b8f3211a2c2d7dc09b7dcbf4c280cb29de
Darrryaa <159245758+Darrryaa@users.noreply.github.com>
2024-03-20 10:10:20 +0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,32 +1,33 @@
 import tkinter as tk
-import math 
+import math
+import ru_local as ru
 
 
 def add_digit(operation, digit, input_l, label = None):
     match operation:
         case "add":
-            curent = input_l.get() 
-            input_l.delete(0, tk.END) 
+            curent = input_l.get()
+            input_l.delete(0, tk.END)
             input_l.insert(0, curent + str(digit))
         case "erase":
             curent = input_l.get()
-            input_l.delete(0, tk.END)  
+            input_l.delete(0, tk.END)
             input_l.insert(0, curent[:-1])
         case "erase_all":
             curent = input_l.get()
-            input_l.delete(0, tk.END) 
-            label.config(text= f"Результат: ")
+            input_l.delete(0, tk.END)
+            label.config(text=(f'{ru.Result}'))
 
 
 def calculate(input_l, label):
     curent = input_l.get()
     try:
-        result = eval(curent) 
-        label.config(text=f"Делить на ноль нельзя!" if error(result) else f"Результат: {result}")
+        result = eval(curent)
+        label.config(text=(f'{ru.You_can_not_divide_by_zero}') if error(result) else f'{ru.Result} {result}')
     except ZeroDivisionError:
-        label.config(text="Делить на ноль нельзя!")
+        label.config(text=(f'{ru.You_can_not_divide_by_zero}'))
     except SyntaxError:
-        label.config(text="Результат: ")
+        label.config(text=(f'{ru.Result}'))
 
 
 def error(result):
